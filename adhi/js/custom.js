cb0b249b69cd44f896370f3aacddc01779e45f15
lightint <obamenzoghobilly@gmail.com>
2019-03-08 08:11:43 +0100

    maj des fichiers

---
+++ adhi/js/custom.js
@@ -0,0 +1,11 @@
+//Navbar load
+
+$( "#head" ).load( "partials/header.html" );
+
+//Navbar load
+
+$( "#head1" ).load( "partials/tiny-head.html" );
+
+//Footer
+
+$(".footer").load("partials/footer.html");
