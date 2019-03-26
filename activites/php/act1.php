180969ea8ea19dd8a59c9caf5ae735b2af128862
lightint <obamenzoghobilly@gmail.com>
2019-03-26 11:05:37 +0100

    ACT1

--- activites/php/act1.php
+++ activites/php/act1.php
@@ -1,3 +1,32 @@
-<?= 'OKACODE' ?>
+<!doctype html>
+<html lang="en">
 
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title><?= 'ACT1' ?></title>
+  </head>
+  <body>
+
+  	<div class="container-fluid" id="app">
+      <div class="row justify-content-md-center">
+        <div class="col-md-auto">
+          <div class="card" style="width: 18rem;">
+            <img src="http://www.bluthemes.com/assets/img/blog/12/bullseye.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <!-- Affichage OKA -->
+              <h5 class="card-title text-center"><?= 'OKACODE' ?></h5>
+            </div>
+          </div>
+
+        </div>
+      </div>
+  	</div>
+
+  </body>
+</html>
